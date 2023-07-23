359bc7703796f5aa0b087e7d9aa771143bf2362f
atten-tutu <139550501+atten-tutu@users.noreply.github.com>
2023-07-24 00:35:24 +0800

    Create multiple_table.c

---
+++ class/multiple_table.c
@@ -0,0 +1,13 @@
+//九九乘法表
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i <= 9;i++)
+	{
+		for (j = 1; j <= i; j++)
+			printf("%d * %d= %-2d ", i, j, i * j);
+		printf("\n");
+	}
+	return 0;
+}
